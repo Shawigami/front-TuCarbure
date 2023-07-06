af07e0e51ff0e200a83c50c2c29bdc35fa3732fb
Julian <juju.leroy47@gmail.com>
2023-07-06 17:04:36 +0200

    feature/linkNewDatabase: add google map button

--- lib/ViewModel/StatementViewModel.dart
+++ lib/ViewModel/StatementViewModel.dart
@@ -33,4 +33,26 @@ class StatementAPI {
       throw Exception('Failed to fetch InfoCarbu from API');
     }
   }
+
+  Future<ValueNotifier<List<Statement>>> fetchInfoStatementsById(int id) async {
+    // Créer un client HTTP en désactivant la vérification du certificat
+    http.Client client = http_io.IOClient(
+      HttpClient()..badCertificateCallback = (cert, host, port) => true,
+    );
+
+    final response = await client.get(Uri.parse(apiUrl+id.toString()));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      statements = List<Statement>.from(
+          jsonData.map((data) => Statement.fromJson(data)));
+      ValueNotifier<List<Statement>> Allstatements = ValueNotifier<List<Statement>>(
+          statements
+      );
+      print(statements);
+      return Allstatements;
+    } else {
+      throw Exception('Failed to fetch InfoCarbu from API');
+    }
+  }
 }
--- lib/views/StationDetailsPage.dart
+++ lib/views/StationDetailsPage.dart
@@ -4,10 +4,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; // Assurez-vous d'importer 'intl' pour utiliser DateFormat
 import 'package:tucarbure/models/Statement.dart';
-
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:android_intent_plus/android_intent.dart';
+import '../ViewModel/StatementViewModel.dart';
 import '../ViewModel/StationViewModel.dart';
 import '../models/Statement.dart';
 import '../models/Station.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import '../models/StationFuel.dart';
 
 class StationDetailsPage extends StatefulWidget {
@@ -22,20 +27,21 @@ class StationDetailsPage extends StatefulWidget {
 class _StationDetailsPageState extends State<StationDetailsPage> {
   late TextEditingController _priceController;
 
+  var viewmodelstatement = StatementAPI();
+  late ValueListenable<List<Statement>> statements ;
 
-
-  var viewmodelstation = StationAPI();
-  late ValueListenable<List<StationAll>> stations;
-
-  void _fetchInfoStations() async {
-    stations = (await viewmodelstation.fetchInfoStations()) as ValueListenable<List<StationAll>>;
+  void _fetchInfoStatements() async {
+    var statementsFetched = await viewmodelstatement.fetchInfoStatementsById(widget.station.station.idStation);
+    setState(() {
+      statements = statementsFetched;
+    });
   }
 
 
   @override
-  void initState() {
-    _fetchInfoStations();
-    super.initState();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _fetchInfoStatements();
   }
 
   @override
@@ -44,13 +50,30 @@ class _StationDetailsPageState extends State<StationDetailsPage> {
     super.dispose();
   }
 
+  void _openGoogleMapsIntent(BuildContext context) async {
+    final url = 'https://www.google.com/maps/search/?api=1&query=${widget.station.station.latitude},${widget.station.station.longitude}';
+    final AndroidIntent intent = AndroidIntent(
+      action: 'action_view',
+      data: Uri.encodeFull(url),
+      package: 'com.google.android.apps.maps',
+    );
+
+    try {
+      await intent.launch();
+    } catch (e) {
+      // Vous pouvez remplacer ceci par une gestion d'erreur personnalisÃ©e
+      print('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Détails de ${widget.station.brand.name}', style: TextStyle(color: Color(0xFFFFFFFF))),
         backgroundColor: Color(0xFF001931),
-        actions: <Widget>[
+        actions:
+        <Widget>[
           Tooltip(
             message: "Ajouter la Station au Favoris",
             child: IconButton(
@@ -64,20 +87,57 @@ class _StationDetailsPageState extends State<StationDetailsPage> {
                 //toggleFavorite(widget.station);
               //},
             ),
+
           ),
+
+
+          
         ],
+
+
       ),
-      body: ListView(
+      body: Column(
         children: [
-          Text("coucou")
-        ],
+      Expanded(
+      child: Container(
+      decoration: BoxDecoration(
+      border: Border.all(
+      color: Colors.grey[300]!,
+        width: 1.0,
       ),
+      borderRadius: BorderRadius.circular(8.0),
+    ),
+    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+    child: ClipRRect(
+    borderRadius: BorderRadius.circular(8.0),
+    child: FlutterMap(
+    options: MapOptions(
+    center: LatLng(widget.station.station.latitude, widget.station.station.longitude),
+    zoom: 17.50,
+    ),
+
+    ),
+    ),
+    ),
+
+    ),
+          FloatingActionButton.extended(
+            onPressed: () => _openGoogleMapsIntent(context),
+            label: Text('Ouvrir dans Google Maps'),
+            icon: Icon(Icons.map),
+            backgroundColor: Color(0xFF001931),
+          )
+    ],
+
+      ),
+
     );
+    
   }
 
-  //bool isFavorite(Station station) {
-  //  return favoriteStations.value.contains(station);
-  //}
+  
+    //bool isFavorite(Station station) {
+    //  return favoriteStations.value.contains(station);
+    //}
 
-
-}
+  }
